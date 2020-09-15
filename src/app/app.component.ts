38db094ae1a7a58c65e4d67113c8b90c4b3f8f20
Rishi <rishikesh.a.j@gmail.com>
2020-09-15 18:13:05 -0500

    changed color scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,32 @@
-import { Component, NgModule } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-
+import { Component, NgModule } from "@angular/core";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 @NgModule({
-  imports: [MatDialogModule, MatDialog]
+  imports: [MatDialogModule, MatDialog],
 })
-
-
-
 export class AppComponent {
-  title = 'personal-porfolio';
-  constructor(public dialog: MatDialog) { }
-
+  title = "personal-porfolio";
+  frontEndSkills = ["Angular", "HTML5", "Bootstrap", "CSS"];
+  BackEndSkills = ['.NET Core', 'PHP', 'Laravel','Flask'];
+  LanguageSkills = ['C#', 'SQL', 'Typescript','Node.Js'];
+  ToolSkills = ['Git','Jira','Selenium','Figma','Postman']
+  constructor(public dialog: MatDialog) {}
 
   openDialog(e: string) {
-    console.log(e, 'this is the log');
-    if (e === 'About') {
+    console.log(e, "this is the log");
+    if (e === "About") {
       const dialogRef = this.dialog.open(DialogContentAboutPage);
-      dialogRef.afterClosed().subscribe(result => {
+      dialogRef.afterClosed().subscribe((result) => {
         console.log(`Dialog result: ${result}`);
       });
-    } else if (e === 'Android') {
+    } else if (e === "Android") {
       const dialogRef = this.dialog.open(DialogContentExampleDialogComponent);
-      dialogRef.afterClosed().subscribe(result => {
+      dialogRef.afterClosed().subscribe((result) => {
         console.log(`Dialog result: ${result}`);
       });
     }
@@ -35,44 +34,43 @@ export class AppComponent {
 }
 
 @Component({
-  selector: 'app-dialog-content-example-dialog',
-  templateUrl: './app-dialog-content-example-dialog.html',
+  selector: "app-dialog-content-example-dialog",
+  templateUrl: "./app-dialog-content-example-dialog.html",
 })
-
-
 export class DialogContentExampleDialogComponent {
-
-  imageObject: Array<object> = [{
-    image: './assets/angular.png',
-    thumbImage: './assets/angular.png',
-    alt: 'alt of image',
-    title: 'title of image'
-  }, {
-    image: './assets/angular.png',
-    thumbImage: './assets/angular.png',
-    title: 'Image title',
-    alt: 'Image alt'
-  }
+  imageObject: Array<object> = [
+    {
+      image: "./assets/angular.png",
+      thumbImage: "./assets/angular.png",
+      alt: "alt of image",
+      title: "title of image",
+    },
+    {
+      image: "./assets/angular.png",
+      thumbImage: "./assets/angular.png",
+      title: "Image title",
+      alt: "Image alt",
+    },
   ];
 }
 
 @Component({
-  selector: 'About',
-  templateUrl: './About.html',
+  selector: "About",
+  templateUrl: "./About.html",
 })
-
 export class DialogContentAboutPage {
-
-  imageObject: Array<object> = [{
-    image: './assets/angular.png',
-    thumbImage: './assets/angular.png',
-    alt: 'alt of image',
-    title: 'title of image'
-  }, {
-    image: './assets/angular.png',
-    thumbImage: './assets/angular.png',
-    title: 'Image title',
-    alt: 'Image alt'
-  }
+  imageObject: Array<object> = [
+    {
+      image: "./assets/angular.png",
+      thumbImage: "./assets/angular.png",
+      alt: "alt of image",
+      title: "title of image",
+    },
+    {
+      image: "./assets/angular.png",
+      thumbImage: "./assets/angular.png",
+      title: "Image title",
+      alt: "Image alt",
+    },
   ];
 }
